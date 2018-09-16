3925096095211a6c39b6a2fbede8b970a07c09da
esteban <estebanequis@gmail.com>
2018-09-16 12:09:31 -0300

    Endpoint (GET/PUT) /api/Clients/{:id} Session Token Validate

--- Backend/src/business-logic/client.logic.js
+++ Backend/src/business-logic/client.logic.js
@@ -7,24 +7,37 @@
 const clientDA = require('../data-access/client-da');
 const rutValidator = require('../services/client-rut-validation.service')
 
-function getClient(id, cb) {
-    clientDA.getClient(id, (err, client) => {
-        return cb(err, client);
+function getClient(dataToSearch, cb) {
+
+    clientDA.userBelongsToCompany(dataToSearch, (result) => {
+        if (result == true) {
+            clientDA.getClient(dataToSearch.id, (err, client) => {
+                return cb(err, client);
+            });
+        } else {
+            return cb('', null);
+        }
     });
 };
 
-function updateClient(clientNewData, cb) {
+function updateClient(dataToSearch, clientNewData, cb) {
 
-    rutValidator.validateRUT(clientNewData.rut, (error, respStatus) => {
-        console.log(error);
-        console.log(respStatus);
+    clientDA.userBelongsToCompany(dataToSearch, (result) => {
+        if (result == true) {
+            rutValidator.validateRUT(clientNewData.rut, (error, respStatus) => {
+                console.log(error);
+                console.log(respStatus);
 
-        if (!error && respStatus == '200') {
-            clientDA.updateClient(clientNewData, (err, client) => {
-                return cb(err, client, null);
+                if (!error && respStatus == '200') {
+                    clientDA.updateClient(clientNewData, (err, client) => {
+                        return cb(err, client, null);
+                    });
+                } else {
+                    return cb(error, null, respStatus);
+                }
             });
         }else{
-            return cb(error, null, respStatus);
+            return cb('', null);
         }
     });
 }
