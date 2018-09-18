2569c27edd629eaaa02d627dda5661492b5bbb7f
Diego Lauz <diego.lauz@hotmail.com>
2018-09-18 00:51:43 -0300

    Formatter configured

--- BackendTypescript/src/app.ts
+++ BackendTypescript/src/app.ts
@@ -2,16 +2,16 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 
 class App {
-    public app:express.Application;
+    public app: express.Application;
 
     constructor() {
         this.app = express();
         this.config();
     }
 
-    private config():void {
+    private config(): void {
         this.app.use(bodyParser.json());
-        this.app.use(bodyParser.urlencoded({extended: true}));
+        this.app.use(bodyParser.urlencoded({ extended: true }));
     }
 }
 
