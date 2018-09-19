51574db95135e2fa9ef03412bc60cd3e44916a97
Diego Lauz <diego.lauz@hotmail.com>
2018-09-19 17:34:13 -0300

    Session service implemented with JWT

--- BackendTypescript/src/config.ts
+++ BackendTypescript/src/config.ts
@@ -1,6 +1,7 @@
 export default {
     DB_URL: 'mongodb://admin:passw0rd@ds161062.mlab.com:61062/devops',
     PORT: process.env.PORT || 3000,
+    JWT_SECRET: process.env.JWT_SECRET || "7eb74c33-140d-4083-8809-e5e6a0eac1e4",
     COMPANY_LOGIN_API_URL: 'https://dev-ops-ort.herokuapp.com/api/auth/token',
     CUSTOMER_RUT_VALIDATE_API_URL: 'https://dev-ops-ort.herokuapp.com/api/rut/validate'
 }
---
+++ BackendTypescript/src/services/session.service.ts
@@ -0,0 +1,21 @@
+import config from "../config";
+import ICredentials from "../models/credentials.model";
+import * as jwt from "jsonwebtoken";
+import { ISession } from "./isession.service";
+
+const session : ISession = {
+    signCredentials(credentials: ICredentials, options?:jwt.SignOptions, callback?: jwt.SignCallback): string {
+        const defaultJwtOptions :jwt.SignOptions = {
+            expiresIn: 8600
+        };
+        const jwtOptions = options == undefined ? defaultJwtOptions : options;
+
+        const encodedCredentials = jwt.sign(credentials.username, config.JWT_SECRET, jwtOptions);
+        return encodedCredentials;
+    },
+    verifyCredentials(encodedCredentials:string, options?:jwt.VerifyOptions, callback?:jwt.VerifyCallback):void {
+        jwt.verify(encodedCredentials, config.JWT_SECRET, options, callback);
+    }
+}
+
+export default session;
