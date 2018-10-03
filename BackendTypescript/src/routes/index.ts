7629cfad6367716488793016d1d9eb436dbc0031
Diego Lauz <diego.lauz@hotmail.com>
2018-10-03 12:39:47 -0300

    Renamed clients route file

--- BackendTypescript/src/routes/index.ts
+++ BackendTypescript/src/routes/index.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import enterpriseLogin from "./enterprise-login.route";
-import clientRoutes from "./client.route";
+import clientRoutes from "./clients.route";
 
 export default (router: Router) => {
     enterpriseLogin(router);
