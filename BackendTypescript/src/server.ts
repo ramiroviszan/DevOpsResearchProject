83f0ea45e9d531cfd9dfaa0bb34c792ee66ef1d9
Pablo <pabluc@gmail.com>
2018-10-08 21:11:39 -0300

    Add route to business

--- BackendTypescript/src/server.ts
+++ BackendTypescript/src/server.ts
@@ -3,16 +3,20 @@ import config from "./config";
 import * as express from 'express';
 import * as path from 'path';
 
-const fronend = '../../FrontendCustomer/customtoys-customers-app-v2/dist';
+const fronend_customers = '../../FrontendCustomer/customtoys-customers-app-v2/dist';
+const fronend_company = '../../FrontendBusiness/customtoys-company-app/dist';
 
 console.log("--------------------------------------------------------------");
 console.log("Ruta al frontend customer");
-console.log(path.join(__dirname, fronend));
+console.log(path.join(__dirname, fronend_customers));
 console.log("--------------------------------------------------------------");
 
-app.use('/front/*',express.static(path.join(__dirname, fronend)));
-app.use('/front',express.static(path.join(__dirname, fronend)));
-app.use('/',express.static(path.join(__dirname, fronend)));
+app.use('/front/*',express.static(path.join(__dirname, fronend_customers)));
+app.use('/front',express.static(path.join(__dirname, fronend_customers)));
+app.use('/',express.static(path.join(__dirname, fronend_customers)));
+
+app.use('/company/*',express.static(path.join(__dirname, fronend_company)));
+app.use('/company',express.static(path.join(__dirname, fronend_company)));
 
 app.listen(config.PORT, () => {
     console.log('Server listening on port: ' + config.PORT);
