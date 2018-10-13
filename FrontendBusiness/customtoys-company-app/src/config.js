6cfdbdbeb6e26eb5a72ae4100ad8c975dcb43cd9
Pablo <pabluc@gmail.com>
2018-10-13 20:31:47 -0300

    Modify route to use just http and add header Content-Type

--- FrontendBusiness/customtoys-company-app/src/config.js
+++ FrontendBusiness/customtoys-company-app/src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    apiUrl: 'https://app-obli-devops.herokuapp.com',
+    apiUrl: 'http://app-obli-devops.herokuapp.com',
     ENTERPRISE_AUTH_API_TOKEN: "d774f026-6243-4a14-9696-051329f82987",
     ENTERPRISE_AUTH_API_URL: 'https://dev-ops-ort.herokuapp.com/api/auth/token',
     CUSTOMER_RUT_VALIDATE_API_URL: 'https://dev-ops-ort.herokuapp.com/api/rut/validate'
